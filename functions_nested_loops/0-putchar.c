1ced0187be767fc53dd912712d0ce218a43e9577
Ogtay-Muradov <ogtay.muradov@gmail.com>
2024-02-25 13:23:51 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
-#include "holberton.h"
+#include <unistd.h>
 
 /**
- * main - print "Holberton"
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
