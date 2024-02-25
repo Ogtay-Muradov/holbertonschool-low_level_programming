58fced38b4921e6dc32781e1293b6f7ef9847443
Ogtay-Muradov <ogtay.muradov@gmail.com>
2024-02-25 02:29:29 +0000

    holberton.h 0-holberton.c 1-alphabet.c

--- functions_nested_loops/holberton.h
+++ functions_nested_loops/holberton.h
@@ -1,13 +1,21 @@
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int);
-int _isalpha(int);
-int print_sign(int);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int);
-void print_times_table(int);
+#include "holberton.h"
+
+/**
+ * main - print "Holberton"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
