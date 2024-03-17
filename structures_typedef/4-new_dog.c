6d7d6c0a113fef3e3db89663554ddea07a57141b
Ogtay-Muradov <ogtay.muradov@gmail.com>
2024-03-17 18:25:59 +0000

    all

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,50 +1,40 @@
-#include <stdlib.h>
 #include "dog.h"
-#include <string.h>
-#include <stdio.h>
+
 /**
-  * new_dog- check the code.
-  * @name: name of dog in struct
-  * @age: age of dog in struct
-  * @owner: name of owner of dog in struct
-  * Return: Always 0.
-  */
+ * new_dog - creates new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: new dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int name_l, owner_l, i;
-	dog_t *new_dog1;
+	dog_t *new_dog;
 
-	new_dog1 = malloc(sizeof(dog_t));
-	if (new_dog1 == NULL)
-		return (NULL);
+	new_dog = malloc(sizeof(dog_t));
 
-	name_l = strlen(name);
-	owner_l = strlen(owner);
-
-	new_dog1->name = malloc(name_l + 1);
-	if (new_dog1->name == NULL)
+	if (!new_dog)
 	{
-		free(new_dog1->name);
-		free(new_dog1);
+		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i < name_l; i++)
-		new_dog1->name[i] = name[i];
-	new_dog1->name[i] = '\0';
 
-	new_dog1->age = age;
+	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
 
-	new_dog1->owner = malloc(owner_l + 1);
-	if (new_dog1->owner == NULL)
+	if (!new_dog->name || !new_dog->owner)
 	{
-		free(new_dog1->owner);
-		free(new_dog1->name);
-		free(new_dog1);
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i < owner_l; i++)
-		new_dog1->owner[i] = owner[i];
-	new_dog1->owner[i] = '\0';
 
-	return (new_dog1);
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return (new_dog);
 }
+
